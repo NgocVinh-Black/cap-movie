96b495b873ccccd41cff96f44b5665bb465ff5dc
ngoc_vinh <ngocvinh1923@gmail.com>
2023-12-31 15:29:30 +0700

    'updateUserManager'

--- src/pages/UserManager/UserManager.js
+++ src/pages/UserManager/UserManager.js
@@ -1,9 +1,115 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { Space, Table, Tag } from "antd";
+import { userServ } from "../../services/userServ";
+import { Button, Modal } from "antd";
 const UserManager = () => {
+  const columns = [
+    {
+      title: "Số thứ tự",
+      dataIndex: "taiKhoan",
+      key: "taiKhoan",
+      render: (text, record, index) => {
+        return <p>{index + 1}</p>;
+      },
+    },
+    {
+      title: "Tài khoản",
+      dataIndex: "taiKhoan",
+      key: "taiKhoan",
+    },
+    {
+      title: "Mật khẩu",
+      dataIndex: "matKhau",
+      key: "matKhau",
+    },
+    {
+      title: "Họ tên",
+      dataIndex: "hoTen",
+      key: "hoTen",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Số điện thoại",
+      dataIndex: "soDT",
+      key: "soDT",
+    },
+    {
+      title: "Thao tác",
+      key: "thaoTac",
+      dataIndex: "thaoTac",
+      render: (_, record) => (
+        <div className="space-x-2">
+          <button>
+            <i className="px-3 py-2 bg-green-600 rounded-md fa-solid fa-pen" />
+          </button>
+          <button
+            onClick={() => {
+              userServ
+                .deleteUser(record.taiKhoan)
+                .then(() => {
+                  userServ.getAllUser().then((res) => {
+                    setListUser(res.data.content);
+                  });
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+            }}
+          >
+            <i className="px-3 py-2 bg-red-600 rounded-md fa-solid fa-trash" />
+          </button>
+        </div>
+      ),
+    },
+  ];
+  const [listUser, setListUser] = useState([]);
+  useEffect(() => {
+    userServ
+      .getAllUser()
+      .then((res) => {
+        setListUser(res.data.content);
+      })
+      .catch();
+  }, []);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <div>UserManager</div>
-  )
-}
+    <div>
+      <Button type="primary" onClick={showModal}>
+        Open Modal
+      </Button>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+      <h2 className="font-bold text-2xl mb-5">Danh sách người dùng</h2>
+      <Table
+        columns={columns}
+        dataSource={listUser}
+        pagination={{
+          pageSize: 10,
+        }}
+      />
+    </div>
+  );
+};
 
-export default UserManager
+export default UserManager;
--- src/services/userServ.js
+++ src/services/userServ.js
@@ -4,4 +4,12 @@ export const userServ = {
   loginServ: (data) => {
     return https.post("/api/QuanLyNguoiDung/DangNhap", data);
   },
+  getAllUser: () => {
+    return https.get("/api/QuanLyNguoiDung/LayDanhSachNguoiDung?MaNhom=GP08");
+  },
+  deleteUser: (taiKhoan) => {
+    return https.delete(
+      `/api/QuanLyNguoiDung/XoaNguoiDung?TaiKhoan=${taiKhoan}`
+    );
+  },
 };
